9163799811ee87485662f21007935e1ae02e210c
BnykDeda22 <115083620+BnykDeda22@users.noreply.github.com>
2023-01-13 15:51:05 +0300

    Add files via upload

---
+++ matr.py
@@ -0,0 +1,26 @@
+from multiprocessing import Pool
+import numpy as np
+
+
+def write(matrix):
+    matrix = np.array(matrix).reshape(len(reader_matrix(1)), len(reader_matrix(2)[1]))
+    with open('result.txt', 'a') as f:
+        f.write(str(matrix) + " ")
+
+
+def multiplication(x, y):
+    return sum(i * k for i, k in zip(x, y))
+
+
+def reader_matrix(number):
+    matrix = []
+    with open(f"matrix{number}.txt", 'r') as f:
+        for line in f.readlines():
+            matrix.append(list(map(int, line.split(', '))))
+    return matrix
+
+
+if __name__ == '__main__':
+    with Pool(processes=4) as pool:
+        matrix = pool.starmap(multiplication, [(i, k) for i in reader_matrix(1) for k in zip(*reader_matrix(2))])
+        write(matrix)
